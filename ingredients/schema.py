b5f9218443b7a08bc78fb54ea4bad21d0c7d1219
samiul <samiul@digitalaptech.com>
2019-01-22 17:19:39 +0530

    query is working properly

--- ingredients/schema.py
+++ ingredients/schema.py
@@ -1,6 +1,5 @@
 import graphene
 from graphene_django.types import DjangoObjectType
-from graphene_django.types import DjangoObjectType
 from ingredients.models import Category, Ingredient
 
 
@@ -15,7 +14,9 @@ class IngredientType(DjangoObjectType):
 
 
 class Query(object):
+    category = graphene.Field(CategoryType, id=graphene.Int(), name=graphene.String())
     all_categories = graphene.List(CategoryType)
+    ingredient = graphene.Field(IngredientType, id=graphene.Int(), name=graphene.String(), note=graphene.String())
     all_ingredients = graphene.List(IngredientType)
 
     def resolve_all_categories(self, info, **kwargs):
@@ -23,4 +24,23 @@ class Query(object):
 
     def resolve_all_ingredients(self, info, **kwargs):
         return Ingredient.objects.select_related('category').all()
+
+    def resolve_category(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+        if id is not None:
+            return Category.objects.get(pk=id)
+        if name is not None:
+            return Category.objects.get(name=name)
+        return None
     
+    def resolve_ingredient(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return Ingredient.objects.get(pk=id)
+        if name is not None:
+            return Ingredient.objects.get(name=name)
+        
+        return None
